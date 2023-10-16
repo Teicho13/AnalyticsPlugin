66c91ce20466ae22b63c4bb143436ce81f2c7f24
Tycho <t.charko@outlook.com>
2023-10-16 14:42:35 +0200

    Add Plugin and change ignore

---
+++ DataAnalytics/Source/DataAnalytics/Private/DataAnalyticsBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DataAnalyticsBPLibrary.h"
+#include "DataAnalytics.h"
+
+UDataAnalyticsBPLibrary::UDataAnalyticsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UDataAnalyticsBPLibrary::DataAnalyticsSampleFunction(float Param)
+{
+	return -1;
+}
+
